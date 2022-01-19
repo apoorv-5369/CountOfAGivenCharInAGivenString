d766300e9c2f906a03acd42fb87cd21aaee5f13f
Apoorv Varshney <85228469+apoorv-5369@users.noreply.github.com>
2022-01-19 23:34:49 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        int num=0;
+        String s;
+        char x;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a string: ");
+      s=sc.nextLine();
+      System.out.println("Enter a Character: ");
+      x=sc.next().charAt(0);
+      for(int i=0;i<s.length();i++){
+          char c= s.charAt(i);
+          if(c==x) num++;
+      }
+      System.out.println(num);
+    }
+}
